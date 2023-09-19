19bfa2d6cd1b1a3637fddd86eafc2ac58b62afe5
SubhamDhar <subhamdhar159@gmail.com>
2023-09-20 00:38:02 +0530

    updates

--- binary.cpp
+++ binary.cpp
@@ -1,36 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int BINARY_SEARCH(int arr[], int size, int key){
-    int start = 0;
-    int end = size-1;
-    int mid = (start + end)/2;
-
-    while(start<=end){
-        if(arr[mid] == key){
-            return mid;
-        }
-        else if (arr[mid] < key)
+int main(){
+    int a[10]={2,4,6,7,8,9,10,17,15},key,l=0,h=9,mid;
+    cout<<"Enter the key element :---\n";
+    cin>>key;
+    while(l<=h){
+        mid=(l+h)/2;
+        if(key==a[mid])
         {
-            start = mid + 1;
+            cout<<"Item found:--\n"<<mid;
+            return 0;
+        }
+        else if (key<a[mid])
+        {
+            h=mid-1;
         }
         else{
-            end = mid -1;
+            l=mid+1;
         }
         
     }
-    return -1;
-}
-
-
-int main(void) {
-
-int arr[5] = {20,25,32,54,98};
-int result = BINARY_SEARCH(arr, 5, 54);
-
-if (result == -1)
-    printf("Not found");
-  else
-    printf("Element is found at index ", result);
-return 0;
+    cout<<"Item not found:--\n";
+    return 0;
 }
